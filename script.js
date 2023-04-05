b0389060d01d8578c8d7a37cdc1797f313b28731
alinawaz143 <alinawaz@uflip.com>
2023-04-05 11:10:29 -0500

    complete assignment

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ let population = 332915073;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
