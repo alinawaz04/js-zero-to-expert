36733e9e49cfe16409cffbbbde7d14b0c211759a
alinawaz143 <alinawaz@uflip.com>
2023-04-05 12:33:33 -0500

    complete challenge

---
+++ script.js
@@ -0,0 +1,11 @@
+const markWeight = 78;
+const markHeight = 1.69;
+const johnWeight = 92;
+const johnHeight = 1.95;
+
+const markBMI = markWeight / (markHeight ** 2);
+const johnBMI = johnWeight / (johnHeight ** 2);
+console.log(markBMI, johnBMI);
+
+const markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
