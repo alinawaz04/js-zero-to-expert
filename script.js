a90f97602126c335ed15ada23c3707c8abe05a19
alinawaz143 <alinawaz@uflip.com>
2023-04-05 11:40:24 -0500

    complete assignment

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let country = "United States";
-let continent = "North America";
+const country = "United States";
+const continent = "North America";
 let population = 332915073;
 
 console.log(country);
@@ -13,3 +13,9 @@ console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+
+language = "English";
+
+// changed unchanging variables to const
+
+country = "Europe"; // when attempting to change const, error appears in console
