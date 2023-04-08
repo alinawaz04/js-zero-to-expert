7faa584ebe06980a4140da09fd2d54be228b2c88
alinawaz143 <alinawaz@uflip.com>
2023-04-08 14:34:21 -0500

    complete assignment

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ console.log('19' - '13' + 17); // 23
 console.log('123' < 57); // false
 console.log(5 + 6 + '4' + 9 - 4 - 2); // 1143
 
-*/
+
 
 
 const numNeighbors = Number(prompt('How many neighbour countries does your country have?'));
@@ -59,3 +59,14 @@ if (numNeighbors === 1) console.log("only 1 border");
 else if (numNeighbors > 1) console.log("more than one border");
 else console.log("no borders");
 
+*/
+const language = "English";
+const population = 332915073;
+const country = "The United States";
+
+
+if (language === "English" && population < 50000000) {
+    console.log(`You should live in ${country}`);
+} else {
+    console.log(`${country} does not meet your criteria`);
+}
