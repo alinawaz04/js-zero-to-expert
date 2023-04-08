30bb177554b64016e9d84674ea13ecb44be7252c
alinawaz143 <alinawaz@uflip.com>
2023-04-08 16:05:45 -0500

    complete challenge

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-const dolphinsAvg = (97 + 112 + 101) / 3;
-const koalasAvg = (109 + 95 + 106) / 3;
-const minScore = 100;
+const bill = 275;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
 
-if (dolphinsAvg > koalasAvg && dolphinsAvg >= minScore) {
-    console.log("Dolphins win");
-} else if (koalasAvg > dolphinsAvg && koalasAvg >= minScore) {
-    console.log("Koalas win");
-} else if (koalasAvg === dolphinsAvg && dolphinsAvg >= minScore && koalasAvg >= minScore) {
-    console.log("It's a draw");
-} else console.log("minimum score not met");
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value was ${bill + tip}`);
