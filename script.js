c0ca9ac9ff39857721be4fb99b271e5047c1a39d
alinawaz143 <alinawaz@uflip.com>
2023-04-08 13:23:07 -0500

    complete challenge

--- script.js
+++ script.js
@@ -7,5 +7,8 @@ const markBMI = markWeight / (markHeight ** 2);
 const johnBMI = johnWeight / (johnHeight ** 2);
 console.log(markBMI, johnBMI);
 
-const markHigherBMI = markBMI > johnBMI;
-console.log(markHigherBMI);
+if (markBMI > johnBMI) {
+    console.log(`Mark's BMI(${markBMI}) is higher than John's BMI(${johnBMI})`)
+} else (
+    console.log(`John's BMI(${johnBMI}) is higher than Mark's BMI(${markBMI})`)
+)
