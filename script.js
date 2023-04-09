c7e5e5f24a924c3a4ff4e210d0b9c57905e81ea4
alinawaz143 <alinawaz@uflip.com>
2023-04-09 13:54:33 -0500

    complete challenge

--- script.js
+++ script.js
@@ -151,7 +151,6 @@ console.log(china);
 
 russia = describePopulation("Russia", 145);
 console.log(us);
-*/
 
 
 
@@ -174,4 +173,22 @@ function checkWinner(koalas, dolphins) {
 
 checkWinner(koalasAvg, dolphinsAvg);
 
+*/
+
+function calcTip(value) {
+    if (value <= 300 && value >= 50) {
+        return value * 0.15;
+    } else return value * 0.20;
+}
+
+
+
+const bills = [125, 555, 44];
+console.log("Bills: ", bills);
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2]),];
+console.log("Tips: ", tips);
+
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log("Total: ", total);
 
