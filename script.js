fcc56c14d7276f8a1a95466e0cb26d712234a850
alinawaz143 <alinawaz@uflip.com>
2023-04-09 14:10:54 -0500

    complete assignment

--- script.js
+++ script.js
@@ -184,3 +184,10 @@ const myCountry = {
     neighbors: ["Canada", "Mexico"]
 };
 
+console.log(`${myCountry.country} has ${myCountry.population} ${myCountry.language}-speaking people, ${myCountry.neighbors.length} neighboring countries and a capital called ${myCountry.capital}`);
+
+myCountry.population += 2000000;
+console.log(myCountry.population);
+
+myCountry["population"] -= 2000000;
+console.log(myCountry.population);
