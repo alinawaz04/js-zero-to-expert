44cc67a0600e8b3d20f64d73c7a4d023780ee49c
alinawaz143 <alinawaz@uflip.com>
2023-04-22 14:22:51 -0500

    complete assignment

--- script.js
+++ script.js
@@ -1,56 +1,41 @@
 "use strict";
-
-// Scoping in Practice
-
-/*
-    1) What are the three types of scoping?
-
-    function scope, block scope and global scope
-
-    2) What makes var different from let and const?
-
-    var is function scoped and are subject to hoisting.
-    let and const are block scoped and are not subject to hoisting
-
-    3) Pick 3 variables with different scopes and label what scope they are.
-
-    global scope:
-    let globalVar = "I am a global variable"
-
-    function myFunction() {
-        function scope:
-        let functionVar = "I am a function variable"
-    }
-
-    block scope:
-    if(true) {
-        const blockVar = "I am a block-scoped variable"
-    }
-
-*/
-
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
-
-  function printAge() {
-    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
-    console.log(output);
-
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      const firstName = "Bob";
-      const str = `Oh, and you're a millenial, ${firstName}`;
-      console.log(str);
-
-      function add(a, b) {
-        return a + b;
+//         Problem: why is this.borderCountries undefined
+const country = {
+  name: "United States",
+  borderCountries: 2,
+  printDescription: function () {
+    const isIsland = function () {
+      if (this.borderCountries != 0) {
+        //Why is this.borderCountries undefinded
+        return false;
+      } else {
+        return true;
       }
-    }
-  }
+    };
 
-  printAge();
+    // console.log(`${this.name} is an island ${isIsland()}`);
+  },
+};
 
-  return age;
-}
+// country.printDescription();
 
-const firstName = "Ali";
-calcAge(1991);
+// this.borderCOuntries is undefined becuase the function "isIsland" is a regular function which creates its own scope, which causes this to refer to the global window object, which is undefined. To fix this, we simply need to use an arrow function instead.
+
+// Solution:
+const countryFixed = {
+  name: "United States",
+  borderCountries: 2,
+  printDescription: function () {
+    const isIsland = () => {
+      if (this.borderCountries != 0) {
+        return false;
+      } else {
+        return true;
+      }
+    };
+
+    console.log(`${this.name} is an island ${isIsland()}`);
+  },
+};
+
+countryFixed.printDescription();
