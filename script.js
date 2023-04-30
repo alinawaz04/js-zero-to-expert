41492aacc165d4e9200d27e51c0842ab3f8ad17f
alinawaz143 <alinawaz@uflip.com>
2023-04-30 14:41:49 -0500

    complete challenge

--- script.js
+++ script.js
@@ -1,144 +1,44 @@
-const game = {
-  team1: "Bayern Munich",
-  team2: "Borrussia Dortmund",
-  players: [
-    [
-      "Neuer",
-      "Pavard",
-      "Martinez",
-      "Alaba",
-      "Davies",
-      "Kimmich",
-      "Goretzka",
-      "Coman",
-      "Muller",
-      "Gnarby",
-      "Lewandowski",
-    ],
-    [
-      "Burki",
-      "Schulz",
-      "Hummels",
-      "Akanji",
-      "Hakimi",
-      "Weigl",
-      "Witsel",
-      "Hazard",
-      "Brandt",
-      "Sancho",
-      "Gotze",
-    ],
-  ],
-  score: "4:0",
-  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
-  date: "Nov 9th, 2037",
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
+const poll = {
+  question: "What is your favourite programming language?",
+  options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+  // This generates [0, 0, 0, 0]. More in the next section!
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const language = Number(
+      prompt(
+        "What is your favourite programming language? \n 0: JavaScript \n 1: Python \n 2: Rust \n 3:++ \n (Write option number)"
+      )
+    );
+    switch (language) {
+      case 0:
+        this.answers[0]++;
+        break;
+      case 1:
+        this.answers[1]++;
+        break;
+      case 2:
+        this.answers[2]++;
+        break;
+      case 3:
+        this.answers[3]++;
+        break;
+      default:
+        alert("Not a valid option");
+    }
+    this.displayResults();
+    this.displayResults("string");
+  },
+  displayResults(type = "array") {
+    if (type === "array") console.log(this.answers);
+    else if (type === "string") {
+      console.log(`Poll results are ${this.answers.join(", ")}`);
+    }
   },
 };
-//                          CHALLENGE 4
-document.body.append(document.createElement("textarea"));
-document.body.append(document.createElement("button"));
+const pollBtn = document.querySelector(".poll");
+pollBtn.addEventListener("click", poll.registerNewAnswer.bind(poll));
 
-const camelCase = function (str) {
-  const strL = str.toLowerCase().trim();
-  const index = strL.indexOf("_") + 1;
-  return (
-    strL.slice(0, index - 1) +
-    strL[index].toUpperCase() +
-    strL.slice(index + 1).trim()
-  );
-};
-
-const camelCaseLines = function (str, char, num) {
-  const lines = str.split("\n");
-  const camelCaseLines = lines.map((line, index) => {
-    const suffix = char.repeat(num * (index + 1));
-    return camelCase(line) + suffix;
-  });
-  return camelCaseLines.join("\n");
-};
-
-const btn = document.querySelector("button");
-const text = document.querySelector("textarea");
-btn.addEventListener("click", function () {
-  console.log(camelCaseLines(text.value, "✅", 1).trim());
-});
-/*
-
-underscore_case
- first_name
-Some_Variable
-  calculate_AGE
-delayed_departure
-
-
-//                          CHALLENGE 2
-
-// 1)
-const scoreEntries = game.scored.entries();
-for (const [el, p] of scoreEntries) {
-  console.log(`Goal ${el + 1}: ${p}`);
-}
-
-// 2)
-const oddValues = Object.values(game.odds);
-let sum = 0;
-for (const val of oddValues) {
-  sum += val;
-}
-avgOdd = sum / oddValues.length;
-console.log("Average Odds:", avgOdd);
-
-// 3)
-console.log(`Odd of victory ${game.team1}: ${game.odds.team1}`);
-console.log(`Odd of draw: ${game.odds.x}`);
-console.log(`Odd of victory ${game.team2}: ${game.odds.team2}`);
-
-// 4)
-const scorers = {};
-for (const player of game.scored) {
-  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
-}
-console.log(scorers);
-   //                       CHALLENGE 1
-//          1.
-const [players1, players2] = game.players;
-console.log("   1.", players1, players2);
-
-//          2.
-const [gk, ...fieldPlayers] = players1;
-console.log("   2.", gk, fieldPlayers);
-
-//          3.
-const allPlayers = [...players1, ...players2];
-console.log("   3.", allPlayers);
-
-//          4.
-const players1Final = [...players1, ...["Thiago", "Coutinho", "Perisic"]];
-console.log("   4.", players1Final);
-
-//          5.
-const { team1, x: draw, team2 } = game.odds;
-console.log("   5.", team1, draw, team2);
-
-//          6.
-const printGoals = function (...names) {
-  for (let i = 0; i < names.length; i++) {
-    console.log(names[i]);
-  }
-  console.log(names.length);
-};
-console.log("   6.");
-printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
-printGoals(...game.scored);
-
-//          7.
-console.log("   7.");
-
-team1 < team2 && console.log("Team 1 more likely to win");
-team2 < team1 && console.log("Team 2 more likely to win");
-
-*/
+const displayResultsBonus = poll.displayResults.call(
+  { answers: [5, 2, 3] },
+  "string"
+);
