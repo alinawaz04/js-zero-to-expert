32a951105b7bcc5086396d5bff78ba1727e959bd
alinawaz143 <alinawaz@uflip.com>
2023-05-15 16:53:36 -0500

    complete challenge

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*
+//          TODO challenge 1
 const Car = function (make, speed) {
   this.make = make;
   this.speed = speed;
@@ -25,3 +27,38 @@ mercedes.accelerate();
 console.log(mercedes);
 mercedes.decelerate();
 console.log(mercedes);
+*/
+
+//          TODO challenge 2
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+  }
+
+  brake() {
+    this.speed -= 5;
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const ford = new CarCl("Ford", 120);
+console.log(ford);
+ford.accelerate();
+console.log(ford);
+ford.brake();
+console.log(ford);
+console.log(ford.speedUS);
+ford.speedUS = 50;
+console.log(ford);
